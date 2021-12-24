85f5419f71fe40ab2fd43637ba6dbba4c8ed5132
Gresse Baptiste <Baptiste.gresse@epitech.digital>
2021-12-24 19:20:52 +0100

    import project 3

---
+++ script.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById('menu');
+let menu = document.getElementById('menusecret');
+let btnf = document.getElementById('btn');
+
+
+btn.addEventListener('click', ()=> {
+    btn.classList.toggle('active');
+    menu.classList.toggle('active');
+    body.classList.toggle('active');
+
+
+    setTimeout(() => {
+        
+    } 3000);
+});
+
+btnf.addEventListener('click', ()=> {
+    btn.
+});
+
